9140cf9c04db6a59bf534ef4f8b4b921241ef714
Pavan-kumar-R06 <pavankumar060905@gmail.com>
2026-06-17 01:58:05 +0530

    fix controller filename

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectAdmin }from "../middleware/auth.js";
-import { getAllBookings, getAllShows, getDashboardData, isAdmin} from "../controllers/admincontroller.js";
+import { getAllBookings, getAllShows, getDashboardData, isAdmin} from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
 
